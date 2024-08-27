3d400061a88daeaf926f2a7f3c1b2fbc2383db67
Edmund Miller <git@edmundmiller.dev>
2024-08-27 14:11:04 -0500

    Update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,7 @@ export default defineConfig({
       sidebar: [
         {
           label: "Getting Started",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
+          autogenerate: { directory: "guides" },
         },
         {
           label: "Projects",
