24f0564c636b62133f0b8c1b02fa2d50f9b22c5d
Edmund Miller <git@edmundmiller.dev>
2024-08-28 09:21:56 -0500

    fix: Try to split up schemas
    https://github.com/withastro/docs/blob/main/src/content/config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,33 +8,40 @@ function removeDupsAndLowerCase(array: string[]) {
   return Array.from(distinctItems);
 }
 
+export const baseSchema = z.object({
+  description: z.string(),
+});
+
+// TODO
+// Ideally I'd like to have a "docs", and a "projects" schema, and make the schema stricter.
+// This will work for now though
+export const projectSchema = baseSchema.extend({
+  repo: z.string().url().optional, // FIXME Remove optional
+  tags: z
+    .array(z.string())
+    .default([])
+    .transform(removeDupsAndLowerCase)
+    .optional(),
+  // FIXME Can probably fetch this with the repo...
+  lastCommitDate: z
+    .string()
+    .or(z.date())
+    .transform((val) => new Date(val))
+    .optional(),
+  language: z.string().optional(), // FIXME Remove optional
+  doi: z.string().url().optional(),
+  // NOTE Gonna need to add more of these
+  // TODO Remove optional
+  license: z.enum(["MIT", "GPL-3", "GPL", null]).optional(),
+  mentor: z.string().optional(),
+  adopter: z.string().optional(),
+  newHome: z.string().url().optional(),
+});
+
+export const docsCollectionSchema = z.union([baseSchema, projectSchema]);
+
 export const collections = {
   docs: defineCollection({
-    schema: docsSchema({
-      extend: z.object({
-        description: z.string(),
-        repo: z.string().url(),
-        // type: z.enum(["tutorial", "guide", "reference"]).optional(),
-        tags: z
-          .array(z.string())
-          .default([])
-          .transform(removeDupsAndLowerCase)
-          .optional(),
-        // FIXME Can probably fetch this with the repo...
-        lastCommitDate: z
-          .string()
-          .or(z.date())
-          .transform((val) => new Date(val))
-          .optional(),
-        language: z.string(),
-        doi: z.string().url().optional(),
-        // NOTE Gonna need to add more of these
-        // TODO Remove optional
-        license: z.enum(["MIT", "GPL-3", "GPL", null]).optional(),
-        mentor: z.string().optional(),
-        adopter: z.string().optional(),
-        newHome: z.string().url().optional(),
-      }),
-    }),
+    schema: docsSchema({ extend: docsCollectionSchema }),
   }),
 };
